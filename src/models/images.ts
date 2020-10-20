5e96c499c2b121aec50e9512ebe4986625de9ee5
luizfelipedev-agillity <73117542+luizfelipedev-agillity@users.noreply.github.com>
2020-10-20 03:47:40 -0300

    Update images.ts

--- src/models/images.ts
+++ src/models/images.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn,ManyToOne,JoinColumn } from "typeorm";
+import {Entity, Column, PrimaryGeneratedColumn , ManyToOne , JoinColumn } from "typeorm";
 import Orphanage from './Orphanage';
 @Entity('images')
 export default class Image{
